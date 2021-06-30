23850a8854e302b87090d446a5e458b0c2f0ffa7
Yves Candau <ycandau@gmail.com>
2021-06-30 03:45:41 +0000

    Added event listener for stdin and callback to exit process.

--- play.js
+++ play.js
@@ -1,3 +1,26 @@
+const { stdin } = require('process')
 const { connect } = require('./client')
 
 connect()
+
+// setup interface to handle user input from stdin
+
+const setupInput = () => {
+  const stdin = process.stdin
+  stdin.setRawMode(true)
+  stdin.setEncoding('utf8')
+  stdin.resume()
+
+  stdin.on('data', handleUserInput)
+  return stdin
+}
+
+const handleUserInput = (ch) => {
+  switch (ch) {
+    case '\u0003':
+      console.log('Thanks for using me, ciao!')
+      return process.exit()
+  }
+}
+
+setupInput()
