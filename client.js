d5a5b8f52d3ce3735a14c57c3a44fdf2ea2918a2
Yves Candau <ycandau@gmail.com>
2021-06-30 01:27:33 +0000

    Added event listener to send name on connection

--- client.js
+++ client.js
@@ -10,6 +10,11 @@ const connect = () => {
   // Interpret incoming data as text
   conn.setEncoding('utf8')
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server.')
+    conn.write('Name: YC')
+  })
+
   // Log incoming data
   conn.on('data', (data) => console.log(data))
 
