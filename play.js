8fe856a720ccea9d6fd37390d93b8d2713be4fea
Yves Candau <ycandau@gmail.com>
2021-06-30 00:51:37 +0000

    Added event listener to log incoming data

--- play.js
+++ play.js
@@ -1,17 +1,20 @@
-const net = require('net');
+const net = require('net')
 
 // Establish connection with the game server
 const connect = () => {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541,
-  });
+  })
 
   // Interpret incoming data as text
-  conn.setEncoding('utf8');
+  conn.setEncoding('utf8')
 
-  return conn;
-};
+  // Log incoming data
+  conn.on('data', (data) => console.log(data))
 
-console.log('Connecting ...');
-connect();
+  return conn
+}
+
+console.log('Connecting ...')
+connect()
